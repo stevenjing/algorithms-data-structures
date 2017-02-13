10504afe59287b13e41d1042f5b7a1fa86584cb6
Steven Jing <steven.jing@gmail.com>
2017-02-13 15:22:28 -0800

    Fix bug and add quick sort info

--- quickSort.js
+++ quickSort.js
@@ -1,5 +1,7 @@
 // quickSort
 // recursively sorts an array of numbers based partitioning via a pivot
+// Time Complexity: Worst Case O(n2), Average Case O(n * log n)
+// Space Complexity: O(1)
 
 function quickSort(nums, start = 0, end) {
   end = end === undefined ? nums.length - 1 : end;
@@ -8,14 +10,14 @@ function quickSort(nums, start = 0, end) {
   }
   let part = partition(nums, start, end);
   quickSort(nums, start, part-1);
-  quickSort(nums, part, end);
+  quickSort(nums, part+1, end);
   return nums;
 }
 
 function partition(nums, start, end) {
-  let wall = 0;
+  let wall = start;
   let pivot = nums[end];
-  for (let i = 0; i < end; i++) {
+  for (let i = start; i < end; i++) {
     let current = nums[i];
     if (current < pivot) {
       swap(i, wall, nums);
@@ -30,6 +32,7 @@ function swap(i, j, array) {
   let temp = array[i];
   array[i] = array[j];
   array[j] = temp;
+  return array;
 }
 
 let nums = [9, 3, 7, 2, 6, 1, 8, 4, 5];
